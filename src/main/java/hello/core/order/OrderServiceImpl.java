a7226e8ccb8b9c9de77412a368b7e6f3766fd256
ysm <clov7153@gmail.com>
2024-03-25 00:18:36 +0900

    스프링 핵심 원리 - 기본편 - Sec4-7.finish

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemeberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
